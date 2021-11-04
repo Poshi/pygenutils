5e9b10d010a60e96c29af828cabe5ad059b179f3
Poshi <jcamps@gmail.com>
2021-11-04 11:32:53 +0100

    Update: added new symbols to export

--- src/pygenutils/__init__.py
+++ src/pygenutils/__init__.py
@@ -1,9 +1,20 @@
-from .pygenutils import (GenomicRangeSet, NumericRange, NumericRangeSet,
-                         SequenceDict)
+'''Python Genetic Utilities.
+
+Small set of utility methods and scripts to manage genomic data in
+ways that are not usually found in the common tools suites.
+'''
+
+from .pygenutils import (AlignmentFormat, BamFilter, BcfFilter,
+                         GenomicRangeSet, NumericRange, NumericRangeSet,
+                         SequenceDict, VariantFormat)
 
 __all__ = [
     'NumericRange',
     'NumericRangeSet',
     'SequenceDict',
     'GenomicRangeSet',
+    'AlignmentFormat',
+    'VariantFormat',
+    'BamFilter',
+    'BcfFilter',
 ]
