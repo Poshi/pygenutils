7fd6c98340543e69ca507a5457c6b8e5019548a6
Poshi <jcamps@gmail.com>
2021-11-13 18:38:39 +0100

    New: added coverage support

--- build.sh
+++ build.sh
@@ -3,3 +3,6 @@
 . .venv/bin/activate
 python3 -m build
 python3 -m pip install --upgrade "$(find dist -name "*.whl" -printf "%T@ %p\n" | sort -rn | head -1 | cut -f2 -d' ')"
+
+python3 -m unittest discover -s tests
+coverage run -m unittest discover -s tests
